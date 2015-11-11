9df5bc9a6592b25cc65510e0e9308d764183e65c
robertcramer <robertbcramer@icloud.com>
2015-11-11 09:50:19 -0500

    fixed tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
   def test_new_valid_post
-    post = Post.new (title: params[:title],
-                      link: params[:link])
+    post = Post.new(title: "Check this out!",
+                    link: "http://yahoo.com")
+
     assert post.valid?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,27 +3,26 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def test_new_valid_user
-    user = User.new(email: params[:email],
-                    username: params[:username],
-                    password: params[:password])
-    assert user.valid?
+    user = User.new(username: "RCrame55",
+                    email: "rcrame@xbox.com")
+    refute user.valid?
   end
 
   def test_new_user_invalid_email
-    user = User.new(username: params[:username],
-                    password: params[:password])
+    user = User.new(username: "RCrame55",
+                    password: "abc123")
     refute user.valid?
   end
 
   def test_new_user_invalid_username
-    user = User.new(email: params[:email],
-                    password: params[:password])
+    user = User.new(email: "rcrame@xbox.com",
+                    password: "abc123" )
     refute user.valid?
   end
 
   def test_new_user_invalid_password
-    user = User.new(username: params[:username],
-                    email: params[:email])
+    user = User.new(username: "RCrame55",
+                    email: "rcrame@xbox.com")
     refute user.valid?
   end
 end
