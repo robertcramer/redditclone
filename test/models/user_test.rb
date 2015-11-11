db8eb9edfb9c7b8cd2f7778bfd0676b1e15d9323
Darryl Cooper <darryl.cooper12@gmail.com>
2015-11-10 23:43:34 -0500

    tests a user cannot be saved if its empty

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "that a user CANNOT be saved if its empty" do
+  	user = User.new
+  	refute user.save
+  end
+  
 end
