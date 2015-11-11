14b54425cceaaeb9281a371eb66ddb0e97b8b60b
Darryl Cooper <darryl.cooper12@gmail.com>
2015-11-11 00:04:08 -0500

    adds test for user_tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,7 +7,12 @@ class UserTest < ActiveSupport::TestCase
 
   test "that a user CANNOT be saved if its empty" do
   	user = User.new
-  	refute user.save
+  	assert_not user.save
   end
   
+  test "CANNOT save user if email is empty" do
+  	user = User.new
+  	assert_not user.save
+  	
+  end
 end
