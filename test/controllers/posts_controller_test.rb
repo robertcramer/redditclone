465e71ec5f19c7a04bf4499e1931ef8c594dced9
Darryl Cooper <darryl.cooper12@gmail.com>
2015-11-11 23:47:38 -0500

    add posts controller

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
+  setup do
+		@post = posts(:one)
+	end
+
+	# test "logged in users can delete a post" do
+	# 	# assert_difference('Post.count', -1) do
+	# 	delete :destroy, id: @post
+	# # end
+
+	# assert_redirected_to posts_path
   # end
+
+  	test "should show post" do
+	  	get :show, id: @post
+	  	assert_response :success
+  	end
+
+  	test "should get new post" do
+  		get :index
+  		assert_response :success
+  		assert_not_nil assigns(:posts)
+  	end
 end
