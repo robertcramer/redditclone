7d0a9735b6f2476498fec19246a7c06c97dca22c
robertcramer <robertbcramer@icloud.com>
2015-11-12 09:04:31 -0500

    added posts controller test

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def login!
+   session[:user_id] = user(:user_id)
+ end
+
+ test "logged in users can make posts" do
+  login!
+  get :new
+  assert_response 200
+  assert_not_nil assigns(:post)
+ end
+
+  test "non-logged in user can't make post" do
+    get :new
+    assert_redirected_to login_path
+  end
+
+
+
+
 end
