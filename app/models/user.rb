fb4fe9f08b7eaf36463d5ab51a3331af38024965
Darryl Cooper <darryl.cooper12@gmail.com>
2015-11-19 17:27:58 -0500

    add, avatar attached file and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
-
+  has_attached_file :avatar, styles: { thumb: "150x150>" }
+  
   has_many :posts
   
-
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   validates_length_of :username, minimum: 5, maximum: 10
   validates_uniqueness_of :email
   validates_presence_of :email
